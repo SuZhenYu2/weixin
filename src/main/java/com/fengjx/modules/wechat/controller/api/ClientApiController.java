befbf6298ce1014d82e890908f6d3b6a6e6f49df
suzy2 <suzy2@lenovo.com>
2017-11-01 22:40:06 +0800

    增加微信支付接口

--- src/main/java/com/fengjx/modules/wechat/controller/api/ClientApiController.java
+++ src/main/java/com/fengjx/modules/wechat/controller/api/ClientApiController.java
@@ -3,6 +3,8 @@ package com.fengjx.modules.wechat.controller.api;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -37,8 +39,6 @@ public class ClientApiController {
 			if(StringUtils.isEmpty(userId)){
 				throw new Exception(" userId 不能为空 ");
 			}
-
-
 			if(publicAccountService.getAccountByUserId(userId) == null) {
 				throw new Exception(" 未找到相关配置  ");
 			}
@@ -69,13 +69,10 @@ public class ClientApiController {
 				throw new Exception(" userId 不能为空 ");
 			}
 
-
 			if(publicAccountService.getAccountByUserId(userId) == null) {
 				throw new Exception(" 未找到相关配置  ");
 			}
 			WxMpService wxMpService = publicAccountService.getWxMpService(userId);
-			
-			
  			
 			WxMpUser wxMpUser =wxMpService.userInfo(openId, "zh_CN");
 			
@@ -97,15 +94,10 @@ public class ClientApiController {
 			if(StringUtils.isEmpty(userId)){
 				throw new Exception(" userId 不能为空 ");
 			}
-			
-			
 			if(publicAccountService.getAccountByUserId(userId) == null) {
 				throw new Exception(" 未找到相关配置  ");
 			}
 			WxMpService wxMpService = publicAccountService.getWxMpService(userId);
-			
-			
-			 
 			
 			WxMpTemplateMessage wxMpTemplateMessage = new WxMpTemplateMessage();
 			
@@ -116,6 +108,79 @@ public class ClientApiController {
 			
  			
 			return Result.renderSuccess(res);
+			
+		} catch (Exception e) {
+			return Result.renderError(e.getMessage());
+		}
+		
+		
+		
+	}
+	@RequestMapping(value = "${clientApi}/getPayInfo")
+	@ResponseBody
+	public Result getPayInfo(
+			String userId,
+			String openId, 
+			String outTradeNo, 
+			double amt, 
+			String body,
+			String tradeType,
+			String ip, 
+			String notifyUrl) throws IOException {
+		
+		
+		try {
+			if(StringUtils.isEmpty(userId)){
+				throw new Exception(" userId 不能为空 ");
+			}
+			if(publicAccountService.getAccountByUserId(userId) == null) {
+				throw new Exception(" 未找到相关配置  ");
+			}
+			WxMpService wxMpService = publicAccountService.getWxMpService(userId);
+			
+			
+			/*  *//**
+		     * 统一下单(详见http://pay.weixin.qq.com/wiki/doc/api/index.php?chapter=9_1)
+		     * 在发起微信支付前，需要调用统一下单接口，获取"预支付交易会话标识"
+		     * @param openId 支付人openId
+		     * @param outTradeNo 商户端对应订单号
+		     * @param amt 金额(单位元)
+		     * @param body 商品描述
+		     * @param tradeType 交易类型 JSAPI，NATIVE，APP，WAP
+		     * @param ip 发起支付的客户端IP
+		     * @param notifyUrl 通知地址
+		     * @return
+		     *//*
+		    WxMpPrepayIdResult getPrepayId(String openId, String outTradeNo, double amt, String body, String tradeType, String ip, String notifyUrl);
+
+		    *//**
+		     * 该接口调用“统一下单”接口，并拼装JSSDK发起支付请求需要的参数
+		     * 详见http://mp.weixin.qq.com/wiki/7/aaa137b55fb2e0456bf8dd9148dd613f.html#.E5.8F.91.E8.B5.B7.E4.B8.80.E4.B8.AA.E5.BE.AE.E4.BF.A1.E6.94.AF.E4.BB.98.E8.AF.B7.E6.B1.82
+		     * @param openId 支付人openId
+		     * @param outTradeNo 商户端对应订单号
+		     * @param amt 金额(单位元)
+		     * @param body 商品描述
+		     * @param tradeType 交易类型 JSAPI，NATIVE，APP，WAP
+		     * @param ip 发起支付的客户端IP
+		     * @param notifyUrl 通知地址
+		     * @return
+		     *//*
+		    Map<String, String> getJSSDKPayInfo(String openId, String outTradeNo, double amt, String body, String tradeType, String ip, String notifyUrl);
+*/
+			//Map map =wxMpService.getJSSDKPayInfo(openId, outTradeNo, amt, body, tradeType, ip, notifyUrl);
+			Map<String,String> payInfo=new HashMap<>(); 
+			payInfo.put("userId", userId);
+			payInfo.put("openId", openId);
+			payInfo.put("outTradeNo", outTradeNo);
+			payInfo.put("amt", amt+"");
+			payInfo.put("tradeType", tradeType);
+			payInfo.put("ip", ip);
+			payInfo.put("notifyUrl", notifyUrl);
+		  
+			
+			Map<String, String> map   =	wxMpService.getJSSDKPayInfo(payInfo);
+			
+			return Result.renderSuccess(map);
 			
 		} catch (Exception e) {
 			return Result.renderError(e.getMessage());
